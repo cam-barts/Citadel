25bc93e7cd8d622fa0ab427da0254134bdfc441a
Jake Creps <crepschrome@gmail.com>
2019-05-20 13:49:32 -0500

    Update encyclo.py
    More tools added.  Margins improved. Colors changed throughout.

--- encyclo.py
+++ encyclo.py
@@ -8,37 +8,38 @@ init(autoreset=True)
 
 def menu():
     print()
-    print("""███████╗███╗   ██╗ ██████╗██╗   ██╗ ██████╗██╗      ██████╗ ██████╗ ██╗   ██╗
-██╔════╝████╗  ██║██╔════╝╚██╗ ██╔╝██╔════╝██║     ██╔═══██╗██╔══██╗╚██╗ ██╔╝
-█████╗  ██╔██╗ ██║██║      ╚████╔╝ ██║     ██║     ██║   ██║██████╔╝ ╚████╔╝ 
-██╔══╝  ██║╚██╗██║██║       ╚██╔╝  ██║     ██║     ██║   ██║██╔═══╝   ╚██╔╝  
-███████╗██║ ╚████║╚██████╗   ██║   ╚██████╗███████╗╚██████╔╝██║        ██║   
-╚══════╝╚═╝  ╚═══╝ ╚═════╝   ╚═╝    ╚═════╝╚══════╝ ╚═════╝ ╚═╝        ╚═╝   """)
-    print("An encyclopedia of OSINT tools")
+    print(""" ███████╗███╗   ██╗ ██████╗██╗   ██╗ ██████╗██╗      ██████╗ ██████╗ ██╗   ██╗
+ ██╔════╝████╗  ██║██╔════╝╚██╗ ██╔╝██╔════╝██║     ██╔═══██╗██╔══██╗╚██╗ ██╔╝
+ █████╗  ██╔██╗ ██║██║      ╚████╔╝ ██║     ██║     ██║   ██║██████╔╝ ╚████╔╝ 
+ ██╔══╝  ██║╚██╗██║██║       ╚██╔╝  ██║     ██║     ██║   ██║██╔═══╝   ╚██╔╝  
+ ███████╗██║ ╚████║╚██████╗   ██║   ╚██████╗███████╗╚██████╔╝██║        ██║   
+ ╚══════╝╚═╝  ╚═══╝ ╚═════╝   ╚═╝    ╚═════╝╚══════╝ ╚═════╝ ╚═╝        ╚═╝   """)
+    print(" An encyclopedia of OSINT tools")
     print()
-    print(Fore.CYAN + "[1]" + Fore.WHITE + " Recon tools")
-    print(Fore.CYAN + "[2]" + Fore.WHITE + " People search tools")
-    print(Fore.CYAN + "[3]" + Fore.WHITE + " Social media tools")
-    print(Fore.CYAN + "[4]" + Fore.WHITE + " Paste site tools")
-    print(Fore.CYAN + "[5]" + Fore.WHITE + " Dark web tools")
+    print(Fore.CYAN + " [1]" + Fore.WHITE + " Recon tools")
+    print(Fore.CYAN + " [2]" + Fore.WHITE + " People search tools")
+    print(Fore.CYAN + " [3]" + Fore.WHITE + " Social media tools")
+    print(Fore.CYAN + " [4]" + Fore.WHITE + " Paste site tools")
+    print(Fore.CYAN + " [5]" + Fore.WHITE + " Dark web tools")
     print()
-    print(Fore.YELLOW + "Ctrl + C to exit")
+    print(Fore.YELLOW + " Ctrl + C to exit")
     print()
-    directory = input("Select a directory: ")
+    directory = input(" Select a directory: ")
     print()
     if directory == "1":
-        print("Recon tools - all in one OSINT tools that serve multiple functions")
+        print(Fore.YELLOW + " Recon tools" + Fore.WHITE + " - all in one OSINT tools that serve multiple functions")
         print()
-        print("[1]" + " Photon - an incredibly fast crawler designed for OSINT")
-        print("[2]" + " GasMask - an all in one OSINT gathering tool")
-        print("[3]" + " Kamerka - build an interactive map of cameras from Shodan")
-        print("[4]" + " FinalRecon - an OSINT tool for all in one web reconnaissance")
-        print("[5]" + " SpiderFoot - automate OSINT from hundreds of sources")
-        print("[<]" + " Main menu")
+        print(Fore.CYAN + " [1]" + Fore.WHITE + " Photon - an incredibly fast crawler designed for OSINT")
+        print(Fore.CYAN + " [2]" + Fore.WHITE + " GasMask - an all in one OSINT gathering tool")
+        print(Fore.CYAN + " [3]" + Fore.WHITE + " Kamerka - build an interactive map of cameras from Shodan")
+        print(Fore.CYAN + " [4]" + Fore.WHITE + " FinalRecon - an OSINT tool for all in one web reconnaissance")
+        print(Fore.CYAN + " [5]" + Fore.WHITE + " SpiderFoot - automate OSINT from hundreds of sources")
+        print(Fore.CYAN + " [<]" + Fore.WHITE + " Main menu")
         print()
+        # Recon Tools
         def recon():
-            time.sleep(1)
-            toolSelect = input("Select: ")
+            time.sleep(0.5)
+            toolSelect = input(Fore.YELLOW + " Select: " + Fore.WHITE)
             if toolSelect == "1":
                 webbrowser.open('https://github.com/s0md3v/Photon') # Go to Photon GitHub page
             elif toolSelect == "2":
@@ -51,87 +52,95 @@ def menu():
                 webbrowser.open('https://spiderfoot.net') # Go to SpiderFoot homepage
             elif toolSelect == "<":
                 menu()
-            elif toolSelect == "x":
-                exit()
             recon()
         recon()
     if directory == "2":
-        print("People search tools - extract OSINT from multiple databases")
+        print(Fore.YELLOW + " People search tools" + Fore.WHITE + " - extract OSINT from multiple databases")
         print()
-        print("[1]" + " Skiptracer - scrape PII paywall sites on a ramen noodle budget")
-        print("[2]" + " LittleBrother - information gathering tool for EU persons")
-        print("[<]" + " Main menu")
+        print(Fore.CYAN + " [1]" + Fore.WHITE + " Skiptracer - scrape PII paywall sites on a ramen noodle budget")
+        print(Fore.CYAN + " [2]" + Fore.WHITE + " LittleBrother - information gathering tool for EU persons")
+        print(Fore.CYAN + " [3]" + Fore.WHITE + " SocialScan - check email addresses across all social platforms")
+        print(Fore.CYAN + " [4]" + Fore.WHITE + " Sherlock - search username availability across all social platforms")
+        print(Fore.CYAN + " [<]" + Fore.WHITE + " Main menu")
         print()
+        # People tools
         def people():
-            time.sleep(1)
-            toolSelect = input("Select: ")
+            time.sleep(0.5)
+            toolSelect = input(Fore.YELLOW + " Select: " + Fore.WHITE)
             if toolSelect == "1":
                 webbrowser.open('https://github.com/xillwillx/skiptracer') # Go to SkipTracer GitHub page
             elif toolSelect == "2":
                 webbrowser.open('https://github.com/lulz3xploit/LittleBrother') # Go to LittleBrother GitHub page
+            elif toolSelect == "3":
+                webbrowser.open('https://github.com/iojw/socialscan') # Go to SocialScan GitHub page
+            elif toolSelect == "4":
+                webbrowser.open('https://github.com/sherlock-project/sherlock') # Go to Sherlock Github page
             elif toolSelect == "<":
                 menu()
             people()
         people()
     if directory == "3":
+        # Social tools
         def socialMenu():
-            print("Social media tools - collect OSINT from various social media platforms")
+            print(Fore.YELLOW + " Social media tools" + Fore.WHITE + " - collect OSINT from various social media platforms")
             print()
-            print("[1]" + " Facebook")
-            print("[2]" + " Twitter")
-            print("[3]" + " Instagram")
-            print("[4]" + " SnapChat")
-            print("[5]" + " Reddit")
-            print("[6]" + " LinkedIn")
-            print("[7]" + " YouTube")
-            print("[<]" + " Main menu")
+            print(Fore.CYAN + " [1]" + Fore.WHITE + " Facebook")
+            print(Fore.CYAN + " [2]" + Fore.WHITE + " Twitter")
+            print(Fore.CYAN + " [3]" + Fore.WHITE + " Instagram")
+            print(Fore.CYAN + " [4]" + Fore.WHITE + " SnapChat")
+            print(Fore.CYAN + " [5]" + Fore.WHITE + " Reddit")
+            print(Fore.CYAN + " [6]" + Fore.WHITE + " LinkedIn")
+            print(Fore.CYAN + " [7]" + Fore.WHITE + " YouTube")
+            print(Fore.CYAN + " [<]" + Fore.WHITE + " Main menu")
             print()
         socialMenu()
         def social():
-            time.sleep(1)
-            toolSelect = input("Select: ")
+            time.sleep(0.5)
+            toolSelect = input(Fore.YELLOW + " Select: " + Fore.WHITE)
+            print()
             if toolSelect == "1":
-                print("Facebook tools")
+                print(Fore.YELLOW + " Facebook tools")
                 print()
-                print("[1]" + " Entropy - Facebook OSINT Collection and Analysis Tool")
-                print("[<]" + " Main menu")
+                print(Fore.CYAN + " [1]" + Fore.WHITE + " Entropy - Facebook OSINT Collection and Analysis Tool")
+                print(Fore.CYAN + " [<]" + Fore.WHITE + " Main menu")
                 print()
-                facebookSelect = input("Select: ")
+                facebookSelect = input(Fore.YELLOW + " Select: " + Fore.WHITE)
                 if facebookSelect == "1":
                     webbrowser.open("https://github.com/andrew-vii/Entro.py")
                 elif facebookSelect == "<":
                     socialMenu()
             elif toolSelect == "2":
                 print()
-                print("Twitter tools")
+                print(" Twitter tools")
             elif toolSelect == "3":
                 print()
-                print("Instagram tools")
+                print(" Instagram tools")
             elif toolSelect == "4":
                 print()
-                print("SnapChat tools")
+                print(" SnapChat tools")
             elif toolSelect == "5":
                 print()
-                print("Reddit tools")
+                print(" Reddit tools")
             elif toolSelect == "6":
                 print()
-                print("LinkedIn tools")
+                print(" LinkedIn tools")
             elif toolSelect == "7":
                 print()
-                print("YouTube tools")
+                print(" YouTube tools")
             elif toolSelect == "<":
                 menu()
             social()
         social()
     if directory == "4":
-        print("Paste site tools - collect OSINT from multiple paste sites")
+        print(" Paste site tools - collect OSINT from multiple paste sites")
         print()
-        print("[1]" + " Scavenger - a crawler bot for credential leaks")
-        print("[<]" + " Main menu")
+        print(Fore.CYAN + " [1]" + Fore.WHITE + " Scavenger - a crawler bot for credential leaks")
+        print(Fore.CYAN + " [<]" + Fore.WHITE + " Main menu")
         print()
+        # Paste tools
         def paste():
-            time.sleep(1)
-            toolSelect = input("Select: ")
+            time.sleep(0.5)
+            toolSelect = input(Fore.YELLOW + " Select: ")
             if toolSelect == "1":
                 webbrowser.open("https://github.com/rndinfosecguy/Scavenger")
             if toolSelect == "<":
@@ -139,14 +148,15 @@ def menu():
             paste()
         paste()
     if directory == "5":
-        print("Dark web tools - collect OSINT from dark web sources")
+        print(" Dark web tools - collect OSINT from dark web sources")
         print()
-        print("[1]" + " TorBot - dark web OSINT tool")
-        print("[<]" + " Main menu")
+        print(Fore.CYAN + " [1]" + Fore.WHITE + " TorBot - dark web OSINT tool")
+        print(Fore.CYAN + " [<]" + Fore.WHITE + " Main menu")
         print()
+        # Dark web tools
         def dark():
-            time.sleep(1)
-            toolSelect = input("Select: ")
+            time.sleep(0.5)
+            toolSelect = input(Fore.YELLOW + " Select: " + Fore.WHITE)
             if toolSelect == "1":
                 webbrowser.open("https://github.com/DedSecInside/TorBot")
             if toolSelect == "<":
