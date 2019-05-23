e41559b9330600575861e3fd0756726623dc8da4
Jake Creps <crepschrome@gmail.com>
2019-05-23 12:59:23 -0500

    Update citadel.py

--- citadel.py
+++ citadel.py
@@ -4,11 +4,6 @@ from colorama import Fore
 import webbrowser
 import time
 from directories import menu
-from directories import recon
-from directories import people
-from directories import social
-from directories import paste
-from directories import dark
 
 menu()
 
