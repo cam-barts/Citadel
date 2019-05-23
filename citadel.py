8990d4553b8ed7a2f2077447c7d16a3fd089568a
Jake Creps <crepschrome@gmail.com>
2019-05-23 10:39:35 -0500

    Add files via upload

---
+++ citadel.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from colorama import init
+from colorama import Fore
+import webbrowser
+import time
+import menu
+import recon
+import people
+import social
+import paste
+import dark 
+
+init(autoreset=True)
